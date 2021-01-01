e046d214621b6ad2fa2d2e523e6a756fa00e0522
Hussam Radwan <hf.radwan89@gmail.com>
2021-01-01 02:16:42 +0200

    ADD : components folder + CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimeComponent } from './components/time/time.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { MotivationalComponent } from './components/motivational/motivational.component';
+import { NewsComponent } from './components/news/news.component';
+import { CardComponent } from './components/card/card.component';
+import { EventsComponent } from './components/events/events.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent,
+    CalendarComponent,
+    MotivationalComponent,
+    NewsComponent,
+    CardComponent,
+    EventsComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/motivational/motivational.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MotivationalComponent } from './motivational.component';
+
+describe('MotivationalComponent', () => {
+  let component: MotivationalComponent;
+  let fixture: ComponentFixture<MotivationalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MotivationalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MotivationalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
