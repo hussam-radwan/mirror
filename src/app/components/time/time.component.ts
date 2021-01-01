fbef2a01456964c6d44e2873e9149a1ea144ca90
Moody <moody@reiblackbook.com>
2020-12-31 18:27:41 -0600

    implemted the time component to show the time and the date

--- src/app/components/time/time.component.ts
+++ src/app/components/time/time.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-time',
   templateUrl: './time.component.html',
   styleUrls: ['./time.component.css']
 })
-export class TimeComponent implements OnInit {
+export class TimeComponent {
 
-  constructor() { }
+  public now: Date = new Date();
 
-  ngOnInit(): void {
+  constructor() {
+    setInterval(() => {
+      this.now = new Date();
+    }, 1);
   }
 
 }
