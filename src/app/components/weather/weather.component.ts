03ff7c70178a709011d510f65ba5e2f5542b3eb8
Moody <moody@reiblackbook.com>
2021-01-02 09:07:28 -0600

    implemented the polling for the weather component

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { retry, switchMap } from "rxjs/operators";
 import { AccuWeatherForcast } from './forecast.model';
 import { WeatherService } from "./weather.service";
 
@@ -12,6 +13,15 @@ export class WeatherComponent {
 
   constructor(private WeatherService: WeatherService) { }
 
-  forecast: Observable<AccuWeatherForcast> = this.WeatherService.get5DaysForecasts();
+  forecast: Observable<AccuWeatherForcast> = timer(0,30*1000).pipe(
+    switchMap(()=> this.WeatherService.get5DaysForecasts()),
+    retry(2)
+  );
+
+  // of({}).pipe(
+  //   mergeMap(() => this.WeatherService.get5DaysForecasts()),
+  //   delay(60*1000),
+  //   repeat()
+  //   );
 
 }
